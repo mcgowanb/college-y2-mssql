49f6260d54d4f4753674fc7b59f8580e8282e632
Brian McGowan <mcgowan.b@gmail.com>
2017-04-05 12:38:33 +0100

    adding T-SQL CA & tester

---
+++ T-SQL/ca-2 sproc.sql
@@ -0,0 +1,45 @@
+ALTER PROC Assign1
+--external variables
+@ActionID int, @MemberID int, @BetAmount smallmoney, @WalletID int
+AS
+--internal variables
+DECLARE @Odds numeric(18,2), @NoOfBets int, @Balance smallmoney
+--get odds
+SELECT @Odds = Odds
+FROM action_tbl
+WHERE ActionID = @ActionID
+--no of bets
+SELECT @NoOfBets = COUNT(*) 
+FROM bets_tbl
+WHERE MemberID = @MemberID AND MONTH(BetDate) = MONTH(GETDATE())
+-- wallet balance
+SELECT @Balance = Balance 
+FROM wallet_tbl
+WHERE WalletID = @WalletID
+-- too many bets
+IF @NoOfBets >= 5
+BEGIN
+;THROW 50001, 'Sorry bet is refused 5 bets have been made this month', 1
+END
+-- Check balance against bet amount
+IF @Balance < @BetAmount
+BEGIN
+;THROW 50002, 'Sorry bet is refused Wallet Balance is breached',1
+END
+--insert records
+BEGIN TRY
+INSERT INTO bets_tbl (ActionID, MemberID, BetAmount, BetDate, Odds)
+VALUES(@ActionID, @MemberID, @BetAmount, GETDATE(), @Odds)
+END TRY
+BEGIN CATCH
+;THROW
+END CATCH
+--update wallet table
+BEGIN TRY
+UPDATE wallet_tbl SET Balance -= @BetAmount WHERE WalletID = @WalletID 
+END TRY
+BEGIN CATCH
+;THROW
+END CATCH
+--complete
+;THROW 50003, 'Order Inserted, Thank You',1
---
+++ proc-test/proc-test/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace proc_test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        S00165159TSQLProjectEntities1 db;
+        public MainWindow()
+        {
+            InitializeComponent();
+            db = new S00165159TSQLProjectEntities1();
+        }
+
+        private void btnAddBet_Click(object sender, RoutedEventArgs e)
+        {
+            int customerID = Convert.ToInt32(txtCustomerID.Text);
+            int walletID = Convert.ToInt32(txtWalletID.Text);
+            int actionID = Convert.ToInt32(txtActionID.Text);
+            decimal amount = Convert.ToDecimal(txtAmount.Text);
+
+            try
+            {
+                //SqlCommand cmd = new SqlCommand("Assign1");
+                //cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                //cmd.Parameters.Add(new SqlParameter("@ActionID", actionID));
+                //cmd.Parameters.Add(new SqlParameter("@CustomerID", customerID));
+                //cmd.Parameters.Add(new SqlParameter("@betAmount", amount));
+                //cmd.Parameters.Add(new SqlParameter("@walltID", walletID));
+
+                //cmd.ExecuteNonQuery();
+                db.AssignTest(actionID, customerID, amount, walletID);
+               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.InnerException.Message);
+            }
+
+
+
+
+        }
+    }
+}
+public static class ExceptionExtensions
+{
+    public static Exception GetOriginalException(this Exception ex)
+    {
+        if (ex.InnerException == null) return ex;
+
+        return ex.InnerException.GetOriginalException();
+    }
+}
